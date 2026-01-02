3c70a550df6ae467a25905c13d8a5cf93f226d1a
Idris Mukthar <idrismukthar6@gmail.com>
2026-01-02 16:32:37 +0100

    fix: remove duplicate header from theme engine

--- src/components/themes/themeEngine.tsx
+++ src/components/themes/themeEngine.tsx
@@ -4,38 +4,22 @@ import { GlassVue } from "./GlassVue";
 import { MonoRaw } from "./MonoRaw";
 import { ForestEcho } from "./ForestEcho";
 
-export function TicketRenderer({ theme, data, profile, type, rangeLabel, orientation }: any) {
-  // Common header to show Category and Timeframe on every ticket
-  const TicketHeader = () => (
-    <div className="w-full mb-4 px-2 flex justify-between items-end border-b border-white/10 pb-2">
-      <div className="flex flex-col">
-        <span className="text-[8px] font-black opacity-40 tracking-[0.3em] uppercase">Itinerary</span>
-        <span className="text-xs font-black italic text-[#00FFF0] uppercase">{type}</span>
-      </div>
-      <div className="flex flex-col text-right">
-        <span className="text-[8px] font-black opacity-40 tracking-[0.3em] uppercase">Validity</span>
-        <span className="text-[10px] font-bold whitespace-nowrap">{rangeLabel}</span>
-      </div>
-    </div>
-  );
+export function TicketRenderer({ theme, ...props }: any) {
+  switch (theme) {
+    case 'NEO_SYNTH':
+      return <NeoSynth {...props} />;
+    case 'GLASS_VUE':
+      return <GlassVue {...props} />;
+    case 'MONO_RAW':
+      return <MonoRaw {...props} />;
+    case 'FOREST_ECHO':
+      return <ForestEcho {...props} />;
+    case 'CYBER_PUNK':
+    default:
+      return <CyberPunk {...props} />;
 
-  const props = { data, profile, type, rangeLabel, orientation };
-
-  const renderTheme = () => {
-    switch (theme) {
-      case 'NEO_SYNTH': return <NeoSynth {...props} />;
-      case 'GLASS_VUE': return <GlassVue {...props} />;
-      case 'MONO_RAW': return <MonoRaw {...props} />;
-      case 'FOREST_ECHO': return <ForestEcho {...props} />;
-      case 'CYBER_PUNK':
-      default: return <CyberPunk {...props} />;
-    }
-  };
-
-  return (
-    <div className="flex flex-col items-center">
-      <TicketHeader />
-      {renderTheme()}
-    </div>
-  );
+      /*case '30_BillionBoys' :
+      return <30BillionBoys {...props} />; */
+      
+  }
 }
